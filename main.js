050846075409fae855c66e203f740bc0a05c2dab
Nino Shubitidze <132759949+Nin15@users.noreply.github.com>
2025-05-09 16:33:10 +0400

    cors

--- main.js
+++ main.js
@@ -1,23 +1,25 @@
 const express = require("express");
-const app = express();
-app.use(express.json());
-const userRouter = require("./users/users.router");
-const connectToDb = require("./db/db");
-const authRouter = require("./auth/auth.route");
-const isAuth = require("./middleware/isAuth.middleware");
-const postRouter = require("./posts/posts.router");
 const cors = require("cors");
-connectToDb();
 
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
+const app = express();
 app.use(
   cors({
     origin: "http://localhost:5173",
     credentials: true,
   })
 );
+app.use(express.json());
+const userRouter = require("./users/users.router");
+const connectToDb = require("./db/db");
+const authRouter = require("./auth/auth.route");
+const isAuth = require("./middleware/isAuth.middleware");
+const postRouter = require("./posts/posts.router");
+connectToDb();
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
 app.use("/posts", isAuth, postRouter);
 app.use("/auth", authRouter);
 app.use("/users", userRouter);
