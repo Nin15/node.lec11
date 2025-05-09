8c8416caa9e20d291ac6a32e028c54b7801c5bf5
Nino Shubitidze <132759949+Nin15@users.noreply.github.com>
2025-05-09 16:40:00 +0400

    sds

--- main.js
+++ main.js
@@ -1,31 +1,34 @@
 const express = require("express");
 const cors = require("cors");
+const serverless = require("serverless-http");
 
 const app = express();
+
 app.use(
   cors({
     origin: "http://localhost:5173",
     credentials: true,
   })
 );
+app.options("*", cors());
+
 app.use(express.json());
-const userRouter = require("./users/users.router");
+
 const connectToDb = require("./db/db");
+connectToDb();
+
+const userRouter = require("./users/users.router");
 const authRouter = require("./auth/auth.route");
 const isAuth = require("./middleware/isAuth.middleware");
 const postRouter = require("./posts/posts.router");
-connectToDb();
 
 app.get("/", (req, res) => {
   res.send("hello world");
 });
 
-app.use("/posts", isAuth, postRouter);
 app.use("/auth", authRouter);
 app.use("/users", userRouter);
+app.use("/posts", isAuth, postRouter);
 
-// app.listen(3000, () => {
-//   console.log("server running on http://localhost:3000");
-// });
-const serverless = require("serverless-http");
+module.exports = app;
 module.exports.handler = serverless(app);
